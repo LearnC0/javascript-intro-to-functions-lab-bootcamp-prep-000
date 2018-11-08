b14e00ca2fc03ed12f0ce58089f0fb73b59ea39f
Spencer Marin <spencermarin@protonmail.com>
2018-11-08 20:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ return console.log('hello')
 
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string)
-return ("I can\'t hear you!") 
+return ('I can\'t hear you!') 
 }
 
 function sayHiToGrandma(string) {
 if (string.toUpperCase() === string)
-return ("YES INDEED!") 
+return ('YES INDEED!') 
 }
 
 function sayHiToGrandma(string) {
