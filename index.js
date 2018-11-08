da9dea1f45c991859704a7667294da30eb2876e5
Spencer Marin <spencermarin@protonmail.com>
2018-11-08 18:43:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout (string) {
+return string.toUpperCase()
+}
+
+function whisper (string) {
+return string.toLowerCase()
+}
+
+function logShout (string) {
+return console.log('HELLO')
+}
+
+function logWhisper (string) {
+return console.log('hello')
+}
+
+function sayHiToGrandma() {
+  return `hello!`
+  console.log()
+}
+sayHiToGrandma('I can\'t hear you!')
+sayHiToGrandma('YES INDEED!')
+sayHiToGrandma('I love you, too.')
