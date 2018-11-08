28303fd308eef0959830f91a0ad83350a989c166
Spencer Marin <spencermarin@protonmail.com>
2018-11-08 20:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ return console.log('hello')
 }
 
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string) 
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
